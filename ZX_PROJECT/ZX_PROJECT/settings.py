009349fe56b4c3b41c3a21ea0f3e1c2671f96ef3
Sam21sop <sbussiness21@gmail.com>
2024-01-22 11:09:48 +0530

    all changes push

--- ZX_PROJECT/ZX_PROJECT/settings.py
+++ ZX_PROJECT/ZX_PROJECT/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'rest_framework',
-    'upload'
+    'upload',
+    'fileuploads'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# fileuploads services
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
